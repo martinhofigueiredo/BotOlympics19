992bd248a5e37f0e0605dc9ac428ed901bc73afd
Descasca <diogo.lipe.santos@gmail.com>
2019-02-23 14:54:02 +0000

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,43 @@
 #include <Arduino.h>
 #include <BotFCTUC.h>
 
-//#define TEST
-#define COLORTHRESHOLD = 100;
+#define TrashHole 67
 
+int incomingByte;
 BotFCTUC arlindo = BotFCTUC();
+
 uint16_t RGBC[3];
 int16_t Distance[3];
 int16_t IR;
 
+void flame_test()
+{
+  arlindo.SetIRScale(SCALE_1);
+
+  IRSensorScale_t scale = arlindo.GetIRScale();
+
+
+  IR = arlindo.GetIR();
+  Serial.print("IR = ");
+  Serial.println(IR);
+
+    if(IR > TrashHole)
+      {
+        Serial.println(" FLAME DETEC");
+        arlindo.FanOn();
+      }
+    else
+      {
+        Serial.println(" FLAME NOT DETEC")
+      }
 }
 
 void setup() {
-  Serial.begin(9600);
   arlindo.begin();
+  Serial.begin(9600);
 }
 
-void loop() {
-
+void loop(){
+    flame_test();
+    Serial.print("test");
 }
