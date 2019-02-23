5f54ba9ed5687458fe715ac76af924b952588eb3
martinhofigueiredo <hell51.games@gmail.com>
2019-02-23 04:41:02 +0000

    Added python helper

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,16 @@
 #include <BotFCTUC.h>
 #include <PID_v1.h>
 
-//#define TEST
+#define TEST
 #define DTHRESHOLD 6
 #define DEFAULTSPEED 60
+#define MAXRANGE 100;
 
 BotFCTUC arlindo = BotFCTUC();
 uint16_t RGBC[3];
 int16_t Distance[3];
 int16_t IR;
-double Setpoint, Input, Output, consKp=0.8, consKi=0.05, consKd=0.25;
+double Setpoint, Input, Output, consKp=10, consKi=5, consKd=5;
 PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
 
 
@@ -23,26 +24,44 @@ void navigate(){
   Serial.println(Distance[1]);
   Serial.print("Direita =");
   Serial.println(Distance[2]);
-  arlindo.Move(255*0.6,255*0.6);
-  #else
-  if (Distance[1]==0) Distance[1]=DTHRESHOLD+2;
-  if (Distance[1]>DTHRESHOLD) {
+  Input = (double)Distance[2];
+  myPID.Compute();
+  Serial.print("Output PID = ");
+  Serial.println(Output);
+  Serial.print("Motor Direira = ");
+  Serial.println(DEFAULTSPEED/(Output/255));
+  Serial.print("Motor Esquerda= ");
+  Serial.println(DEFAULTSPEED);
+
+  Serial.write(27);       // ESC command
+  Serial.print("[2J");    // clear screen command
+  Serial.write(27);
+  Serial.print("[H");
+
+  #else/*
+  for(int i = 0; i < 3;i++){
+    if(Distance[i]==0){
+      Distance[i] = MAXRANGE;
+    }
+  }
+
+  if (Distance[2]>DTHRESHOLD) {
+	  arlindo.Move(DEFAULTSPEED,DEFAULTSPEED);
+  }
+  else if (Distance[1]<DTHRESHOLD) { //demasiado perto de merdas
     Input = (double)Distance[2];
     myPID.Compute();
     Serial.println(Output);
     Serial.println(Distance[1]);
     Serial.println(Distance[2]);
-	  arlindo.Move(DEFAULTSPEED*(int16_t)Output,DEFAULTSPEED/(int16_t)Output);
-  }
-  else if (Distance[1]<DTHRESHOLD) { //demasiado perto de merdas
-    arlindo.Move(0,DEFAULTSPEED);				 //roda 90 graus
-  }
+    arlindo.Move(DEFAULTSPEED,DEFAULTSPEED*(Output/255));				 //roda 90 graus
+  }*/
   #endif
 }
 
 void setup() {
 
-  Serial.begin(9600);
+  Serial.begin(57600);
   arlindo.begin();
   Setpoint=(double)DTHRESHOLD;
   myPID.SetMode(AUTOMATIC);
