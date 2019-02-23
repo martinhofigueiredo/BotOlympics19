cd14da2ec710e14494bd03527ade55c4a64ce18f
hmarques97 <hmpmarques25@gmail.com>
2019-02-23 02:07:05 +0000

    pid calibration

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,30 @@
 #include <BotFCTUC.h>
 #include <PID_v1.h>
 
-#define DTHRESHOLD 50
-#define DEFAULTSPEED 50
+//#define TEST
+#define DTHRESHOLD 6
+#define DEFAULTSPEED 60
 
 BotFCTUC arlindo = BotFCTUC();
 uint16_t RGBC[3];
 int16_t Distance[3];
 int16_t IR;
-double Setpoint, Input, Output, consKp=1, consKi=0.05, consKd=0.25;
+double Setpoint, Input, Output, consKp=0.75, consKi=0.05, consKd=0.25;
 PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
 
 
 void navigate(){
-  arlindo.GetSonars(*Distance);
+  arlindo.GetSonars(Distance);
+  #ifdef TEST
+  Serial.print("Esquerda =");
+  Serial.println(Distance[0]);
+  Serial.print("Center =");
+  Serial.println(Distance[1]);
+  Serial.print("Direita =");
+  Serial.println(Distance[2]);
+  #else
+
+
   if (Distance[1]>DTHRESHOLD) {
     Input = (double)Distance[2];
     myPID.Compute();
@@ -26,14 +37,17 @@ void navigate(){
   else if (Distance[1]<DTHRESHOLD) { //demasiado perto de merdas
     arlindo.Move(0,DEFAULTSPEED);				 //roda 90 graus
   }
-
+  #endif
 }
 
 void setup() {
+
   Serial.begin(9600);
   arlindo.begin();
   Setpoint=(double)DTHRESHOLD;
   myPID.SetMode(AUTOMATIC);
+  while(!arlindo.ButtonPressed()){}
+
 }
 
 void loop() {
