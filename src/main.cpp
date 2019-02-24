5e41466da90f5ed300125022f8e091e0abd53ca4
martinhofigueiredo <hell51.games@gmail.com>
2019-02-24 07:27:25 +0000

    Switch case test. help us obiwan you are our only hope

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 #define K 1.5
 
 //NAVIGATION
-#define DEFAULTSPEED 100
+#define DEFAULTSPEED 80
+#define SLOWSPEED DEFAULTSPEED/2
 #define MAXRANGE 100
 #define HARDTURN 97
 #define FRONTLIMIT 14
@@ -26,19 +27,15 @@ int16_t Distance[3];
 int16_t IR;
 int16_t Motor[2];
 
-int incomingByte;
 
-bool oldline = false;
-int REline = 0;
 int n_linhas = 0;
-
+int state = 0;
 unsigned long currentmillis;
 
 
 
 //Wallfollowing algorithm
 void navigate(){
-  arlindo.GetSonars(Distance);
   for(int i = 0; i < 3;i++){
     if(Distance[i]==0){
       Distance[i] = 50;
@@ -61,138 +58,112 @@ void navigate(){
 }
 
 //Returns 2 if flame far, 1 if close, 0 if no flame
-int flametest(){
-  arlindo.SetIRScale(SCALE_8);
-  arlindo.GetSonars(Distance);
-  arlindo.Move(0,0);
-
-  IR = arlindo.GetIR();
-  Serial.print("IR = ");
-  Serial.println(IR);
-  Serial.print("dist = ");
-  Serial.println(Distance[1]);
-
-
-    if(IR > IRLIMIT && Distance[1] > 13)
-      {
-        Serial.println(" FLAME DETEC FAR");
-        arlindo.RedLEDOn();
-        return 2;
-      }
-      if(IR > IRLIMIT && Distance[1] <= 13){
-        Serial.println("FLAME DETEC CLOSE");
-        arlindo.RedLEDOn();
-        return 1;
-    }
-      else
-      {
-        Serial.println(" FLAME NOT DETEC");
-        return 0;
-      }
+bool flametest(){
+  if(IR > IRLIMIT)
+  {
+    Serial.println(" FLAME DETEC");
+    arlindo.RedLEDOn();
+    return 1;
+  }
+  else
+  {
+    Serial.println(" FLAME NOT DETEC");
+    arlindo.RedLEDOff();
+    return 0;
+  }
 }
 
 //true if passes white line
-int linetest(){
-  arlindo.GetColor(RGBC);
+bool linetest(){
   if(RGBC[0] < COLORLIMIT && RGBC[1] < COLORLIMIT && RGBC[2] < COLORLIMIT){
     arlindo.GreenLEDOff();
     return 0;
   }
   else{
+    n_linhas++;
     arlindo.GreenLEDOn();
     return 1;
   }
 }
 
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(57600);
   arlindo.begin();
+  arlindo.SetIRScale(SCALE_8);
   while(!arlindo.ButtonPressed()){}
+
 }
 
 void loop()
 {
-  //flametest();
-  //navigate();
+  arlindo.GetColor(RGBC);
+  IR = arlindo.GetIR();
+  arlindo.GetSonars(Distance);
 
-  if(oldline == false && linetest() == true)
-  {
-    REline = 1;
-    n_linhas++;
-  }
-  else{
-    REline = 0;
-  }
-
-  // Dentro Quarto
-  if ((REline == 1) && !(n_linhas%2))
-  {
-    currentmillis = millis();
-    // primeiro 1 seg
-
-  while(millis()- currentmillis < 1000){
-    navigate();
-
-  }
-// segundo 2 seg //
-  while(millis()- currentmillis < 3500 && millis() - currentmillis > 1000 && n_linhas > 1)
-  {
-      Serial.print("dar a volta ");
-      Serial.println(flametest());
-      arlindo.Move(DEFAULTSPEED*0.8,-DEFAULTSPEED*0.8);
-      if(flametest() == 2 || flametest() == 1){
-        Serial.println("break");
-        break;
+  switch(state){
+    case 0: //A mexer
+    {
+      navigate();
+      if(linetest() && !(n_linhas%2) && n_linhas > 1)
+      {
+        state = 1;
       }
-  }
-  switch(flametest()){
-    case 1:
-    {
-      while(1){
-      arlindo.Move(0,0);}
       break;
     }
-    case 2:
+    case 1: //Dentro de Quarto
     {
-      while(1){arlindo.Move(0,0);}
+      currentmillis = millis();
+      while(millis()- currentmillis < 1000 || Distance[1] < 15){
+        arlindo.Move(SLOWSPEED,SLOWSPEED);
+      }
+      state = 2;
+      break;
+    }
+    case 2: //RODA A PROCURA DA CHAMA
+    {
+      while(millis()- currentmillis < 3500 && millis() - currentmillis > 1000 && n_linhas > 1)
+      {
+          arlindo.Move(DEFAULTSPEED*0.8,-DEFAULTSPEED*0.8);
+          if(flametest() == 1){
+            state = 3;
+            break;
+          }
+      }
+      break;
+    }
+    case 3:
+    {
+
+      while(Distance[1] > 13)
+      {
+      arlindo.Move(SLOWSPEED,SLOWSPEED);
+      }
+      arlindo.Move(0,0);
+      state = 4;
+      break;
+    }
+    case 4:
+    {
+      arlindo.FanOn();
       break;
     }
   }
-  arlindo.Move(0,0);
-  }
-  else{navigate();}
-
-
-  oldline = linetest();
-
   #ifdef TEST
   //Serial.print("Esquerda =");
   //Serial.println(Distance[0]);
-  //Serial.print("Center =");
-  //Serial.println(Distance[1]);
-  //Serial.print("Direita =");
-  //Serial.println(Distance[2]);
-  //Input = (double)Distance[2];
-  //myPID.Compute();
-  //Serial.print("Output PID = ");
-  //Serial.println(Output);
-  /*Serial.print("Motor Esquerda = ");
-  Serial.println(Motor[0]);
-  Serial.print("Motor Direita = ");
-  Serial.println(Motor[1]);*/
-
-
-  Serial.print("LineDetect = ");
-  Serial.println(linetest());
+  Serial.print("Center =");
+  Serial.println(Distance[1]);
+  Serial.print("Direita =");
+  Serial.println(Distance[2]);
+  Serial.print("IR = ");
+  Serial.println(IR);
   Serial.print("n_linhas = ");
   Serial.println(n_linhas);
-  Serial.print(" ! n_linhas % 2 = ");
+  Serial.print("!n_linhas%12 = ");
   Serial.println(!(n_linhas%2));
-  Serial.print("REline = ");
-  Serial.println(REline);
+  Serial.print("State = ");
+  Serial.println(state);
   Serial.println();
-
-
   /*Serial.write(27);       // ESC command
   Serial.print("[2J");    // clear screen command
   Serial.write(27);
